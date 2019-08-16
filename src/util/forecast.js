6d8e8f7748a9294de29d525b52a15ce58168502a
vnaren13 <vnaren13@gmail.com>
2019-08-16 19:27:39 +0530

    new information added

--- src/util/forecast.js
+++ src/util/forecast.js
@@ -14,7 +14,8 @@ const forecast = (latitude,longitude,callback)=>
          console.log('invalid input',undefined)
         }
        else{
-           callback(undefined,response.body.daily.data[0].summary+' It is currently '+ response.body.currently.temperature+' degrees out.There is a '+response.body.currently.precipProbability+'% chance of rain.')
+          
+           callback(undefined,response.body.daily.data[0].summary+' It is currently '+ response.body.currently.temperature+' degrees out.There is a '+response.body.currently.precipProbability+'% chance of rain.\n Maximum temperature '+response.body.daily.data[0].temperatureMax+' . Minimum temperature '+response.body.daily.data[0].temperatureMin)
        }
     })
     
